94571b92d97d6ef7fc8e9df4f9557d0f76ca8a14
Mark M. Florida <4173034+mflorida@users.noreply.github.com>
2023-09-18 14:11:41 -0500

    Update main exports

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 // What is this tomfoolery?
 import d$ from './src/domage.js';
 import dq from './src/dquery.js';
-export { dq } from './src/dquery.js';
-export { d$ } from './src/domage.js';
+export * from './src/dquery.js';
+export * from './src/domage.js';
+export * from './src/domageUtils.js';
+export * from './src/parseTag.js';
 // add dq to d$ object.
 d$.dq = dq;
 d$.dquery = dq;
